949222d037ca904238e107cae028574140a40313
Yin Myat Thwe <yinmyatthwe1224@gmail.com>
2024-02-22 00:28:02 -0800

    モックアップを作成した

--- intern-book-app/src/app/app.component.ts
+++ intern-book-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BooklistComponent } from './pages/booklist/booklist.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:[
+    RouterOutlet,
+    BooklistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
