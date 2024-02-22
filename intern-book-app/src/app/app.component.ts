e1e654c938968e445eebc1cdf7e57b96625f3d81
Yin Myat Thwe <yinmyatthwe1224@gmail.com>
2024-02-22 14:42:01 -0800

    toolbarのコンポーネントを作成する

--- intern-book-app/src/app/app.component.ts
+++ intern-book-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BooklistComponent } from './pages/booklist/booklist.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:[
+    RouterOutlet,
+    BooklistComponent,
+  ToolbarComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
