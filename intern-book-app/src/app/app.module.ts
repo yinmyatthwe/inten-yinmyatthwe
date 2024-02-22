5e1877efde9a0ddb8e719ed8db2462e1b9010e9d
Yin Myat Thwe <yinmyatthwe1224@gmail.com>
2024-02-22 00:28:34 -0800

    モックアップを作成した

---
+++ intern-book-app/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
+import { BooklistComponent } from './pages/booklist/booklist.component';
+
+@NgModule({
+declarations: [
+    
+
+],
+imports: [
+    BrowserModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    AppRoutingModule,
+    AppComponent,
+    BooklistComponent,
+],
+providers: [
+    provideAnimationsAsync()
+],
+bootstrap: []
+})
+export class AppModule { }
