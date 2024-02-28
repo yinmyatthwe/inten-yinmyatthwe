d22b147f3861f6f42c932d6ef735ef563fdd65be
Yin Myat Thwe <yinmyatthwe1224@gmail.com>
2024-02-27 22:44:53 -0800

    issue 5

--- intern-book-app/src/app/pages/booklist/booklist.component.ts
+++ intern-book-app/src/app/pages/booklist/booklist.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,Input, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 import { BOOKS } from '../../types/mock-books';
+import { Book } from '../../types/book';
 import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -24,8 +25,19 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './booklist.component.html',
   styleUrl: './booklist.component.css'
 })
+
 export class BooklistComponent {
   books=BOOKS;
+  selectedBook?:Book;
+
+  onSelect(book:Book):void{
+    this.selectedBook=book;
+  }
+
+
+
+  @Input() book?:Book;
+
 }
 
 
