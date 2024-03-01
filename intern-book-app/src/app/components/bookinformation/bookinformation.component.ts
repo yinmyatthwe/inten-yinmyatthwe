d99ca6715275fe5cb8461b35ac88974689cec65a
Yin Myat Thwe <yinmyatthwe1224@gmail.com>
2024-03-01 12:16:09 -0800

    issue 7: 書籍の削除

--- intern-book-app/src/app/components/bookinformation/bookinformation.component.ts
+++ intern-book-app/src/app/components/bookinformation/bookinformation.component.ts
@@ -7,7 +7,6 @@ import {MatButtonModule} from '@angular/material/button';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { NgIf,NgFor } from '@angular/common';
 import { BookService } from '../../service/book.service';
-import { BookData } from '../../types/book';
 
 @Component({
   standalone: true,
@@ -28,11 +27,10 @@ import { BookData } from '../../types/book';
 
 export class BookinformationComponent {
   @Input() books=BOOKS;
-  book=new BookData();
+
   constructor(private bookService:BookService){}
   
   deleteBook(book:any){
-    this.bookService.deleteBook(book);
-    
+        this.bookService.deleteBook(book);
   }
 }
